6ca6e00c0d3f98ade08fd766f9276debc4dfa7e9
saralaloux <sara.laloux@gmail.com>
2022-07-12 14:36:29 +0200

    add react menu burger

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import "./styles/stylesheets/index.css";
+function App() {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
